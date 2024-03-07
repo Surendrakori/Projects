eca360a188491f1a0c285fe395854f278a0aef76
surendra <surendrakori@lerostech.com>
2024-03-07 17:45:46 +0530

    first commit

---
+++ ToDoList/App.js
@@ -0,0 +1,20 @@
+const listElements=[];
+
+let addToList = ()=>{
+    if(txtBox.value!==""){
+    listElements.push(txtBox.value);}
+    displayList();
+}
+let displayList = () =>{
+    let str="";
+    listElements.forEach((value,index)=>{
+        str+=`<ul><li>${index+1}-${value}
+              <button onclick='deleteElements(${index})'><i class="bi bi-trash-fill"></i></button></li></ul>        
+        `
+    })
+    list.innerHTML=str;
+}
+let deleteElements = (index) =>{
+    delete listElements[index]
+    displayList();
+}
